61dcb891249b0754d2b43c310c1f562502774417
Integer128 <59063176+integer128@users.noreply.github.com>
2020-03-13 12:08:16 +0300

    selector fix

--- src/sources/selector.cpp
+++ src/sources/selector.cpp
@@ -20,7 +20,7 @@ std::pair<DBResult, std::vector<DBEntry> > Selector::selectAll(const QString &ta
             QVariantList entryData;
             entryData.reserve(entryRecord.count());
 
-            for(int i = 0; i > entryRecord.count(); ++i)
+            for(int i = 0; i < entryRecord.count(); ++i)
             {
                 entryData.push_back(entryRecord.value(i));
             }
@@ -45,6 +45,27 @@ std::pair<DBResult, std::vector<DBEntry> > Selector::select(const QString &table
     query = generateQuery(tableName, arguments..., conditions...);
 
     DBResult result;
+    QSqlQuery resultQuery;
+    std::tie(result, resultQuery) = m_executor.execute(query);
+
+    std::vector<DBEntry> returnData;
+
+    if(result == DBResult::OK)
+    {
+        while(resultQuery.next())
+        {
+            const QSqlRecord& entryRecord { resultQuery.record() };
+            QVariantList entryData;
+            entryData.reserve(entryRecord.count());
+
+            for(int i = 0; i < entryRecord.count(); ++i)
+            {
+                entryData.push_back(entryRecord.value(i));
+            }
+            returnData.emplace_back(std::move(entryData));
+        }
+    }
+    return { result, returnData };
 }
 
 QString Selector::generateQuery(const QString &tableName) const
